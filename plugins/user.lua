7e2b1b014b7f5213ccd97fa52957f93784fe8cb1
Jake <jake.comay@gmail.com>
2023-04-27 17:29:16 -0400

    Basic Config

--- plugins/user.lua
+++ plugins/user.lua
@@ -4,9 +4,23 @@ return {
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
+  --   event = { "BufRead" },
+  --   cmd = {},
+  --   keys = { {} },
   --   config = function()
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix", "TodoLocList", "TodoTrouble", "TodoTelescope" },
+    opts = {},
+  },
+  {
+    "smolck/command-completion.nvim",
+    opts = {},
+    lazy = false,
+  }
 }
